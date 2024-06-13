b662e5c675d23e57e2fe1e86f8176363c42deae6
rameshbissu <rameshbissu646@gmail.com>
2024-06-13 14:37:45 +0530

    Reviews - React icons

--- 03-reviews/starter/src/App.jsx
+++ 03-reviews/starter/src/App.jsx
@@ -1,9 +1,13 @@
 import { useState } from 'react';
 import people from './data'
+import {FaBeer} from 'react-icons/fa'
 const App = () => {
   const [index,setIndex] = useState(0)
   const {name,job,image,text} = people[index]
-  console.log(name);
-  return <h2>Reviews Starter</h2>;
+  
+  return <div>
+    <h2>Revies Starter</h2>
+    <FaBeer className='beer'/>
+  </div>;
 };
 export default App;
