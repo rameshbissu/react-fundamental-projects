d313f110b0fef836e5ea0c4bdeb4fdcae87dcdd2
rameshbissu <rameshbissu646@gmail.com>
2024-06-14 07:16:09 +0530

    Menu - unique categories

--- 05-menu/starter/src/App.jsx
+++ 05-menu/starter/src/App.jsx
@@ -3,8 +3,18 @@ import Title from "./Title";
 import menu from "./data";
 import Menu from "./Menu";
 
+// const tempCategories = menu.map((item) => item.category);
+// const tempSet = new Set(tempCategories);
+// const tempItems = ["all", ...tempSet];
+// console.log(tempItems);
+
+const allCategories = ["all", ...new Set(menu.map((item) => item.category))];
+
 const App = () => {
   const [menuItems, setMenuItems] = useState(menu);
+  const [categories, setCategories] = useState(allCategories);
+  console.log(categories);
+
   return (
     <main>
       <section className="menu">
