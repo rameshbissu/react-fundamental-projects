456224dbf1802d835b096884abe86f849e6dc9eb
rameshbissu <rameshbissu646@gmail.com>
2024-06-15 15:14:12 +0530

    Tabs - duties and UUID library

--- 06-tabs/starter/src/Duties.jsx
+++ 06-tabs/starter/src/Duties.jsx
@@ -1,8 +1,21 @@
 import React from "react";
+import { v4 as uuidv4 } from "uuid";
+import { FaAngleDoubleRight } from "react-icons/fa";
 
 const Duties = ({ duties }) => {
-  console.log(duties);
-  return <div>Duties</div>;
+  return (
+    <div>
+      {duties.map((duty, index) => {
+        const id = uuidv4();
+        return (
+          <div key={id} className="job-desc">
+            <FaAngleDoubleRight className="job-icon" />
+            <p>{duty}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Duties;
