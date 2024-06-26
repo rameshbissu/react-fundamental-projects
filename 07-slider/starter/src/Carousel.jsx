854cfc8086066afa8a4c18fb5183f89f4c41bf02
rameshbissu <rameshbissu646@gmail.com>
2024-06-26 08:31:25 +0530

    Slider - structure

---
+++ 07-slider/starter/src/Carousel.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { shortList, list, longList } from "./data";
+import { FaQuoteRight } from "react-icons/fa";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
+
+const Carousel = () => {
+  const [people, setPeople] = useState(shortList);
+  const preSlide = () => {};
+  const nextSlide = () => {};
+  return (
+    <section className="slider-container">
+      {people.map((person) => {
+        const { id, image, name, title, quote } = person;
+        return (
+          <article className="slide" key={id}>
+            <img src={image} alt={name} className="person-img" />
+            <h5 className="name">{name}</h5>
+            <p className="title">{title}</p>
+            <p className="text">{quote}</p>
+            <FaQuoteRight className="icon" />
+          </article>
+        );
+      })}
+      <button className="prev" type="button" onClick={preSlide}>
+        <FiChevronLeft />
+      </button>
+      <button className="next" type="button" onClick={nextSlide}>
+        <FiChevronRight />
+      </button>
+    </section>
+  );
+};
+
+export default Carousel;
