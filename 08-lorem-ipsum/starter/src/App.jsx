45fd556705111eae54ee38165caa4d81df0b0b3e
rameshbissu <rameshbissu646@gmail.com>
2024-06-27 08:24:33 +0530

    Lorem ipsum - structure

---
+++ 08-lorem-ipsum/starter/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import data from "./data";
+
+const App = () => {
+  const [count, setCount] = useState(1);
+  const [text, setText] = useState([]);
+
+  return (
+    <section className="section-center">
+      <h4>tired of boring lorem ipsum?</h4>
+      <form action="" className="lorem-form">
+        <label htmlFor="amount">paragraph</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          min="1"
+          step="1"
+          max="8"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button className="btn" type="submit">
+          generate
+        </button>
+      </form>
+    </section>
+  );
+};
+export default App;
