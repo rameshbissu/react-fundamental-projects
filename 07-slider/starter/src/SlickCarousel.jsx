a7959db0b7a50269e28c81059c554cf9439d9e6f
rameshbissu <rameshbissu646@gmail.com>
2024-06-27 08:08:29 +0530

    Slider - React slick complete

--- 07-slider/starter/src/SlickCarousel.jsx
+++ 07-slider/starter/src/SlickCarousel.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
-import { list } from "./data";
+import { longList } from "./data";
 import { FaQuoteRight } from "react-icons/fa";
 
 const SlickCarousel = () => {
@@ -13,30 +13,22 @@ const SlickCarousel = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
   };
+  const [people, setPeople] = useState(longList);
   return (
-    <section className="slick-container">
-      <h2>slick carousel</h2>;
-      <Slider {...settings}>
-        <div>
-          <h3>1</h3>
-        </div>
-        <div>
-          <h3>2</h3>
-        </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-        <div>
-          <h3>5</h3>
-        </div>
-        <div>
-          <h3>6</h3>
-        </div>
-      </Slider>
-    </section>
+    <Slider {...settings} className="slider-container">
+      {people.map((person) => {
+        const { id, image, name, title, quote } = person;
+        return (
+          <article key={id} className="slide">
+            <img src={image} alt={name} className="person-img" />
+            <h5 className="name">{name}</h5>
+            <p className="title">{title}</p>
+            <p className="text">{quote}</p>
+            <FaQuoteRight className="icon" />
+          </article>
+        );
+      })}
+    </Slider>
   );
 };
 
