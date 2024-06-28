df599c4ddf729d8ab5964d5313317c18dc3091e8
rameshbissu <rameshbissu646@gmail.com>
2024-06-28 16:48:01 +0530

    Sidebar/Modal - components

---
+++ 12-sidebar/starter/src/App.jsx
@@ -0,0 +1,13 @@
+import Home from "./Home";
+import Modal from "./Modal";
+import Sidebar from "./Sidebar";
+
+const App = () => {
+  return <main>
+    <Home/>
+    <Sidebar/>
+    <Modal/>
+    
+    </main>;
+};
+export default App;
---
+++ 12-sidebar/starter/src/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <button type="button" className="btn">Home</button>
+    </div>
+  )
+}
+
+export default Home
