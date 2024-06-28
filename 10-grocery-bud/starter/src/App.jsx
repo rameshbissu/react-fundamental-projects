06e7669d9d3e1e454c2fb5b850ea89592a77249f
rameshbissu <rameshbissu646@gmail.com>
2024-06-28 09:04:08 +0530

    Color Generato - Local Storage

--- 10-grocery-bud/starter/src/App.jsx
+++ 10-grocery-bud/starter/src/App.jsx
@@ -3,8 +3,22 @@ import Form from "./Form";
 import { nanoid } from "nanoid";
 import Items from "./Items";
 
+const getLocalStorage = () => {
+  let list = localStorage.getItem("list");
+  if (list) {
+    list = JSON.parse(localStorage.getItem("list"));
+  } else {
+    list = [];
+  }
+  return list;
+};
+
+const setLocalStorage = (items) => {
+  localStorage.setItem("list", JSON.stringify(items));
+};
+const defaultList = JSON.parse(localStorage.getItem("list") || "[]");
 const App = () => {
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState(defaultList);
 
   const addItem = (itemName) => {
     const newItem = {
@@ -12,13 +26,16 @@ const App = () => {
       completed: false,
       id: nanoid(),
     };
-    setItems([...items, newItem]);
+    const newItems = [...items, newItem];
+    setItems(newItems);
+    setLocalStorage(newItems);
   };
   const removeItem = (itemId) => {
     const newItems = items.filter((item) => {
       return item.id !== itemId;
     });
     setItems(newItems);
+    setLocalStorage(newItems);
   };
 
   return (
