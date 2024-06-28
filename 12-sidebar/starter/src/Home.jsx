e052768e7efe50db66537e3daabab356db72fa1e
rameshbissu <rameshbissu646@gmail.com>
2024-06-28 18:29:32 +0530

    Sidebar/Modal - Global Context

--- 12-sidebar/starter/src/Home.jsx
+++ 12-sidebar/starter/src/Home.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import { useGlobalContext } from './context'
 
 const Home = () => {
+    const {openSidebar,openModal}= useGlobalContext()
+    console.log(openSidebar);
+    console.log(openModal);
   return (
     <div>
         <button type="button" className="btn">Home</button>
