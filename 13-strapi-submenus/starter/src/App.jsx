5f8f2a9185e17206b04263474df95a0a7e48d617
rameshbissu <rameshbissu646@gmail.com>
2024-06-29 08:33:40 +0530

    Strapi - Components

--- 13-strapi-submenus/starter/src/App.jsx
+++ 13-strapi-submenus/starter/src/App.jsx
@@ -1,8 +1,15 @@
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+import Submenu from "./Submenu";
 import { useGlobalContext } from "./context";
 
 const App = () => {
-  const {isSidebarOpen} = useGlobalContext();
-  console.log(isSidebarOpen);
-  return <h2>Strapi Starter</h2>;
+  return <main>
+    <Navbar/>
+    <Hero/>
+    <Sidebar/>
+    <Submenu/>
+  </main>;
 };
 export default App;
