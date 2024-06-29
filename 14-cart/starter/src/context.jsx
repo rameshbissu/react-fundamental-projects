521a703d7590d0d85d86ce9e13f77edffd5ff5d9
rameshbissu <rameshbissu646@gmail.com>
2024-06-29 19:28:06 +0530

    Cart - Remove item

--- 14-cart/starter/src/context.jsx
+++ 14-cart/starter/src/context.jsx
@@ -11,22 +11,27 @@ import {
 } from "./actions";
 import cartItems from "./data";
 
-
 const AppContext = createContext();
 
 const initialState = {
   loading: false,
-  cart: new Map(cartItems.map((item) => [item.id,item])),
+  cart: new Map(cartItems.map((item) => [item.id, item])),
 };
 
 export const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   const clearCart = () => {
-    dispatch({type:CLEAR_CART})
-  }
+    dispatch({ type: CLEAR_CART });
+  };
+  const remove = (id) => {
+    dispatch({type:REMOVE,payload:{id}})
+  };
+
   return (
-    <AppContext.Provider value={{ ...state,clearCart }}>{children}</AppContext.Provider>
+    <AppContext.Provider value={{ ...state, clearCart,remove }}>
+      {children}
+    </AppContext.Provider>
   );
 };
 
