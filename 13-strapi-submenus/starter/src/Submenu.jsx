74b0207a692d556c568e480d9a93c4978f5b6cd1
rameshbissu <rameshbissu646@gmail.com>
2024-06-29 12:18:33 +0530

    Strapi - Hide Submenu (part-2)

--- 13-strapi-submenus/starter/src/Submenu.jsx
+++ 13-strapi-submenus/starter/src/Submenu.jsx
@@ -2,12 +2,27 @@ import React from "react";
 import { useGlobalContext } from "./context";
 import sublinks from "./data";
 
+import { useRef } from "react";
+
 const Submenu = () => {
   const { pageId, setPageId } = useGlobalContext();
   const currentPage = sublinks.find((item) => item.pageId === pageId);
 
+  const submenuContainer = useRef(null);
+  const handleMouseLeave = (e) => {
+    const submenu = submenuContainer.current;
+    const { left, right, bottom } = submenu.getBoundingClientRect();
+    const { clientX, clientY } = e;
+    if (clientX < left - 1 || clientX > right - 1 || clientY > bottom - 1) {
+      setPageId(null);
+    }
+  };
   return (
-    <div className ={currentPage? "submenu show-submenu" : 'submenu'}>
+    <div
+      className={currentPage ? "submenu show-submenu" : "submenu"}
+      onMouseLeave={handleMouseLeave}
+      ref={submenuContainer}
+    >
       <h5>{currentPage?.page}</h5>
       <div
         className="submenu-links"
@@ -19,7 +34,7 @@ const Submenu = () => {
         {currentPage?.links?.map((link) => {
           const { id, url, label, icon } = link;
           return (
-            <a href={url} key={id} >
+            <a href={url} key={id}>
               {icon}
               {label}
             </a>
